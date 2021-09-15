5b0a3b2b2d449c98ac894780837748bb0403b88a
Juan Lucero <luceroweb@gmail.com>
2021-09-15 16:25:29 -0700

    Pulled in header/footer boilerplate from twentynineteen theme

--- header.php
+++ header.php
@@ -5,5 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
   </head>
+
   <body <?php body_class(); ?>>
-    Your header content here.
+  <?php wp_body_open(); ?>
+    <div id="page" class="site">
+      <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'reacttheme' ); ?></a>
+
+        <header id="masthead" class="<?php echo is_singular() ? 'site-header featured-image' : 'site-header'; ?>">
+
+          <div class="site-branding-container">
+            <?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
+          </div><!-- .site-branding-container -->
+
+          <?php if ( is_singular() ) : ?>
+            <div class="site-featured-image">
+              <?php
+                reacttheme_post_thumbnail();
+                the_post();
+                $discussion = ! is_page() ? reacttheme_get_discussion_data() : null;
+
+                $classes = 'entry-header';
+              if ( ! empty( $discussion ) && absint( $discussion->responses ) > 0 ) {
+                $classes = 'entry-header has-discussion';
+              }
+              ?>
+              <div class="<?php echo $classes; ?>">
+                <?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+              </div><!-- .entry-header -->
+              <?php rewind_posts(); ?>
+            </div>
+          <?php endif; ?>
+        </header><!-- #masthead -->
+
+      <div id="content" class="site-content">
---
+++ template-parts/footer/footer-widgets.php
@@ -0,0 +1,16 @@
+<?php
+if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
+
+	<aside class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Footer', 'reacttheme' ); ?>">
+		<?php
+		if ( is_active_sidebar( 'sidebar-1' ) ) {
+			?>
+					<div class="widget-column footer-widget-1">
+					<?php dynamic_sidebar( 'sidebar-1' ); ?>
+					</div>
+				<?php
+		}
+		?>
+	</aside><!-- .widget-area -->
+
+<?php endif; ?>
