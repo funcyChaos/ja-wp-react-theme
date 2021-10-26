07e204b536a687c3c0d816c3958a91f938e45f17
Austin Reilly <areilly@alphaworks.tech>
2021-10-26 16:35:01 -0700

    fixed the hard coded menu items function by using get_nav_menu_name to procure the name from a location. voila

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function reacttheme_load_assets() {
   wp_enqueue_script('ourmainjs', get_theme_file_uri('/build/index.js'), array('wp-element'), '1.0', true);
+	
   wp_enqueue_style('ourmaincss', get_theme_file_uri('/build/index.css'));
 }
 
@@ -15,16 +16,18 @@ function reacttheme_add_support() {
   register_nav_menu('Primary', 'Main Menu');
 }
 
+add_action('after_setup_theme', 'reacttheme_add_support');
+
 function wp_menu_route() {
-  return wp_get_nav_menu_items('Primary'); // Get the array of wp objects, the nav items for our queried location.
+	$mainMenu = wp_get_nav_menu_name('Primary');
+	// Get the array of wp objects, the nav items for our queried location.
+  return wp_get_nav_menu_items($mainMenu);
 }
 
 add_action( 'rest_api_init', function () {
-  //https://your-wp-domain-url.com/wp-json/custom-name/menu
+  //https://your-wp-domain-url.com/wp-json/menu
   register_rest_route( 'menu', '/primary', array(
     'methods' => 'GET',
     'callback' => 'wp_menu_route',
   ) );
 } );
-
-add_action('after_setup_theme', 'reacttheme_add_support');
